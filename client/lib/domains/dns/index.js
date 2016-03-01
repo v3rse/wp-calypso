1816e96a46ed7be5e45773e4d6bc57278b22f488
Radford Smith <contact@radsmith.com>
2016-03-01 13:50:55 -0800

    Domain Management: DNS: Allow root A records to be updated

--- client/lib/domains/dns/index.js
+++ client/lib/domains/dns/index.js
@@ -1,9 +1,13 @@
 /**
  * External dependencies
  */
-const includes = require( 'lodash/includes' ),
-	mapValues = require( 'lodash/mapValues' ),
-	endsWith = require( 'lodash/endsWith' );
+import endsWith from 'lodash/endsWith';
+import filter from 'lodash/filter';
+import find from 'lodash/find';
+import includes from 'lodash/includes';
+import mapValues from 'lodash/mapValues';
+import startsWith from 'lodash/startsWith';
+import without from 'lodash/without';
 
 function validateAllFields( fieldValues, domainName ) {
 	return mapValues( fieldValues, ( value, fieldName ) => {
@@ -118,7 +122,7 @@ function isRootDomain( name, domainName ) {
 }
 
 function canBeRootDomain( type ) {
-	return includes( [ 'MX', 'SRV', 'TXT' ], type );
+	return includes( [ 'A', 'MX', 'SRV', 'TXT' ], type );
 }
 
 function getFieldWithDot( field ) {
@@ -126,7 +130,49 @@ function getFieldWithDot( field ) {
 	return ( typeof field === 'string' && field.match( /^([a-z0-9-_]+\.)+\.?[a-z]+$/i ) ) ? field + '.' : field;
 }
 
-module.exports = {
-	validateAllFields,
-	getNormalizedData
+function isWpcomRecord( record ) {
+	return startsWith( record.id, 'wpcom:' );
+}
+
+function filterRootARecords( domain, records ) {
+	return filter( records, {
+		type: 'A',
+		name: `${domain}.`
+	} );
+}
+
+function removeDuplicateWpcomRecords( domain, records ) {
+	const wpcomRecord = find( filterRootARecords( domain, records ), isWpcomRecord ),
+		customRecord = find( records, { type: 'A', name: '' } );
+
+	if ( wpcomRecord && customRecord ) {
+		return without( records, wpcomRecord );
+	}
+
+	return records;
+}
+
+function addMissingWpcomRecords( domain, records ) {
+	const rootARecords = filterRootARecords( domain, records );
+
+	if ( rootARecords.length === 0 ) {
+		const defaultRootARecord = {
+			domain,
+			id: `wpcom:A:${domain}.:${domain}`,
+			name: `${domain}.`,
+			protected_field: true,
+			type: 'A'
+		};
+
+		return records.concat( [ defaultRootARecord ] );
+	}
+
+	return records;
+}
+
+export {
+	addMissingWpcomRecords,
+	getNormalizedData,
+	removeDuplicateWpcomRecords,
+	validateAllFields
 };
